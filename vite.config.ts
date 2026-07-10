09ddc2ec42943e3a9453ff0cb8ccc6a05ba79082
progmaticAI <progmaticai6@gmail.com>
2026-07-10 10:40:44 +0500

    configure vite to build to build directory

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
   build: {
+    outDir: 'build',
     chunkSizeWarningLimit: 1000,
   },
 })
